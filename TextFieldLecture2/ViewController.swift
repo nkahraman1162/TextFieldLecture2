d91a839b6ded78d481e750740100f724ef27813b
Neslisah Kahraman <nkahraman1162@stu.d214.org>
2020-10-28 15:01:49 -0500

    added 2 textfields, a button and a label to the storyboard (one textfield was programmatically)

--- TextFieldLecture2/ViewController.swift
+++ TextFieldLecture2/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    let textfield2 = UITextField(frame: CGRect(x: 20, y: 100, width: 100, height: 34))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textfield2.borderStyle = .roundedRect
+        
+        view.addSubview(textfield2)
     }
 
-
+    @IBAction func buttonWasPressed(_ sender: Any) {
+    
+    }
+    
 }
 
